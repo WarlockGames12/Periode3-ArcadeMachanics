05e0b86ae55343b12c61523b2609f0b4dce27bf2
WarlockGames12 <fpweeyou@gmail.com>
2021-02-12 10:34:42 +0100

    2D dungeon Crawler

---
+++ Assets/LookAtMouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    }
+}
---
+++ Assets/controlplayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlplayer : MonoBehaviour
+{
+    float speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // What is the player doing with the controls?
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), 0);
+
+        // Update the ships position each frame
+        transform.position += move
+            * speed * Time.deltaTime;
+
+    }
+}
