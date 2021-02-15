aeb6b9f516c73eeec500acde5f8391ddb47efba6
WarlockGames12 <fpweeyou@gmail.com>
2021-02-15 14:53:02 +0100

    Assets & Control

---
+++ Assets/Camera_Follow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform spshipsprite;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(spshipsprite.position.x, spshipsprite.position.y, transform.position.z);
+    }
+}
