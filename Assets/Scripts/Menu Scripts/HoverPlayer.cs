c4ea1ec44bb5b67dd3f76187228e3b79a3f7c716
WarlockGames12 <fpweeyou@gmail.com>
2021-03-04 13:13:41 +0100

    beter aligned

---
+++ Assets/Scripts/Menu Scripts/HoverPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverPlayer : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioClip AudioClip;
+    private AudioSource audioSource;
+
+    //Do this when the cursor enters the rect area of this selectable UI object.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.PlayOneShot(AudioClip);
+    }
+}
