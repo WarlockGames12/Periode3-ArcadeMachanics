468a55611cf639137da059ec9827746f9d763475
WarlockGames12 <fpweeyou@gmail.com>
2021-03-05 18:58:07 +0100

    lighting

--- Assets/Scripts/Menu Scripts/PauseMenu.cs
+++ Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -7,6 +7,12 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
 
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
