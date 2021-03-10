7efad58b2e00778f8737fa6ecc64fad5fbebfde8
WarlockGames12 <fpweeyou@gmail.com>
2021-03-10 14:27:27 +0100

    menu volume and pause sounds

---
+++ Assets/Scripts/Menu Scripts/buttons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
