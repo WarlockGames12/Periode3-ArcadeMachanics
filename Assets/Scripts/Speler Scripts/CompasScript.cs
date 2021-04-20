dd3f846a7d78001f3d5df627f0349718ac735c3b
WarlockGames12 <fpweeyou@gmail.com>
2021-04-20 15:25:01 +0200

    Compas

---
+++ Assets/Scripts/Speler Scripts/CompasScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompasScript : MonoBehaviour
+{
+    public Transform Compas;
+    public float agroRange;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Compas == null)
+        {
+            return;
+        }
+        if (Vector2.Distance(transform.position, Compas.position) < agroRange) //Agro range
+        {  //rotate to look at the player
+            transform.LookAt(Compas.position);
+            transform.Rotate(new Vector2(0, -90), Space.Self);//correcting the original rotation
+        }
+    }
+}
