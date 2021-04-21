ff45b85092e5a80d01ddb98aee8ffe480b734e7f
WarlockGames12 <fpweeyou@gmail.com>
2021-04-21 16:42:19 +0200

    Done

---
+++ Assets/Scripts/Speler Scripts/FPSCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public Text fpsDisplay;
+
+    void Update()
+    {
+        float fps = 1 / Time.unscaledDeltaTime;
+        fpsDisplay.text = "" + fps;
+    }
+}
